1c335b5855c38192e0158b8a6a92b61c2591c3fd
rathorevishnu8161 <166025530+rathorevishnu8161@users.noreply.github.com>
2024-08-02 05:29:37 +0000

    variables

---
+++ basic/01_variable.js
@@ -0,0 +1,14 @@
+// variables in js
+const  customerId = '123455';
+let    customerName = 'Vishnu singh rathore';
+var    customerEmail = "vishnu@gmail.com";
+       customerNumber = 1234567890;
+
+// investigation 
+// customerId = 2;
+customerName = 'Govind Singh Tanwar';
+customerEmail = 'singh@gmail.com';
+customerNumber = 999999;
+
+
+console.table([customerId,customerName,customerEmail,customerNumber]);
