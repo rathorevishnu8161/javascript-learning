c95d043945dff396a054941161271f65851152d1
rathorevishnu8161 <166025530+rathorevishnu8161@users.noreply.github.com>
2024-08-02 09:58:35 +0000

    vari

--- basic/01_variable.js
+++ basic/01_variable.js
@@ -3,12 +3,16 @@ const  customerId = '123455';
 let    customerName = 'Vishnu singh rathore';
 var    customerEmail = "vishnu@gmail.com";
        customerNumber = 1234567890;
+let  customerState ;
 
 // investigation 
+/*
+not use var variable issue of block issue
+*/
 // customerId = 2;
 customerName = 'Govind Singh Tanwar';
 customerEmail = 'singh@gmail.com';
 customerNumber = 999999;
 
 
-console.table([customerId,customerName,customerEmail,customerNumber]);
+console.table([customerId,customerName,customerEmail,customerNumber,customerState]);
