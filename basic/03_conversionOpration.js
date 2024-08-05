68ab2dfce73837732536329f2c0d09b89ae7e205
rathorevishnu8161 <166025530+rathorevishnu8161@users.noreply.github.com>
2024-08-05 10:58:04 +0000

    oprators

---
+++ basic/03_conversionOpration.js
@@ -0,0 +1,49 @@
+/*
+conversion of datatype in javascript
+*/
+// let score = undefined;
+// let scoreValue = Number(score);
+// console.log(typeof (scoreValue));
+// console.log(scoreValue);
+
+/*
+Note :- agr string ki value agr number ho to ye ise Number me convert kr dega nhi to Nan return krega jo ki not a number hota h
+"343"  => 343;
+"343abc" => Nan or Not a Number
+null => 0
+undefined => NaN
+*/
+
+// convert value in boolean
+// let isLogin = 'hello';
+// let booleanisLogin = Boolean(isLogin);
+// console.log(booleanisLogin);
+// console.log(typeof(booleanisLogin));
+
+
+/*
+covert nuber in  string
+*/
+
+// let firstNumber = 342324324;
+// let convertedNumber = String(firstNumber);
+// console.log(convertedNumber);
+// console.log(typeof(convertedNumber));
+
+
+/*  ==============  Opraters  ==================  */
+
+// let str1 = 4;
+// let str2 = -str1;
+// console.log(str2);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**9);
+// console.log(3%2);
+
+// console.log("2"+2+4);
+
+// console.log(typeof(+true));
+// console.log(+false);
